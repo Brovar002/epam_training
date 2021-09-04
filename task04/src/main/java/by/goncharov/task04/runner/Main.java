1e4a89bfd747566abc659c5441b6a8d2e6a4192c
Daniil Goncharov <brovar002@gmail.com>
2021-09-04 13:19:16 +0300

    init(task04): start project

---
+++ task04/src/main/java/by/goncharov/task04/runner/Main.java
@@ -0,0 +1,13 @@
+package by.goncharov.task04.runner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
