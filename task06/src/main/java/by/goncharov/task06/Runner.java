cb8cd741a3f704d96c057752ce99a4517f694645
Daniil Goncharov <brovar002@gmail.com>
2021-09-29 20:30:07 +0300

    init(task06): start task06

---
+++ task06/src/main/java/by/goncharov/task06/Runner.java
@@ -0,0 +1,13 @@
+package by.goncharov.task06;
+
+/**
+ * Hello world!
+ *
+ */
+public class Runner
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
