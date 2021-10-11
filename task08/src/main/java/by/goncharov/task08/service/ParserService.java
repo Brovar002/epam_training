9bab5e6ab6025818837bd5644d6a70ebde281c37
Daniil Goncharov <brovar002@gmail.com>
2021-10-11 20:39:27 +0300

    feat(task08): add xml and ParserService

---
+++ task08/src/main/java/by/goncharov/task08/service/ParserService.java
@@ -0,0 +1,35 @@
+package by.goncharov.task08.service;
+
+import by.goncharov.task08.parser.*;
+
+public final class ParserService {
+
+    private static final ParserService INSTANCE =  new ParserService();
+    private enum TypeParser {
+        SAX, STAX, DOM
+    }
+
+    private ParserService() {
+    }
+
+    public static ParserService getInstance() {
+        return INSTANCE;
+    }
+
+    public AbstractFlowerParser createFlowerParser(final String typeParser)
+            throws ParserException {
+        TypeParser type = TypeParser.valueOf(typeParser.toUpperCase());
+        switch (type) {
+            case DOM:
+                return new DOMFlowerParser();
+            case STAX:
+                return new StAXFlowerParser();
+            case SAX:
+                return new SAXFlowerParser();
+            default:
+                throw new EnumConstantNotPresentException(
+                        type.getDeclaringClass(), type.name());
+        }
+    }
+}
+
