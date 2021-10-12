cfb963cc7e6535d46b0e677e2650ec94914a918a
Daniil Goncharov <brovar002@gmail.com>
2021-10-12 17:52:34 +0300

    feat(task08): add Controller

---
+++ task08/src/main/java/by/goncharov/task08/controller/Controller.java
@@ -0,0 +1,60 @@
+package by.goncharov.task08.controller;
+
+import by.goncharov.task08.parser.AbstractFlowerParser;
+import by.goncharov.task08.parser.ParserException;
+import by.goncharov.task08.service.ParserService;
+import by.goncharov.task08.util.Validator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class Controller extends HttpServlet {
+
+    private static final Logger LOGGER = LogManager.getLogger(
+            Controller.class);
+    @Override
+    protected void doGet(final HttpServletRequest req,
+                         final HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req,
+                          final HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(final HttpServletRequest req,
+                                final HttpServletResponse resp)
+            throws ServletException, IOException {
+        String typeParser = req.getParameter("radios");
+        Part part = req.getPart("file");
+        boolean status = Validator.validate(part.getInputStream());
+        req.setAttribute("status", status);
+        if (status) {
+            try {
+                AbstractFlowerParser parser = ParserService.getInstance()
+                        .createFlowerParser(typeParser);
+                InputStream streamXml = part.getInputStream();
+                parser.buildSetFlowers(streamXml);
+                req.setAttribute("flowers", parser.getFlowers());
+                req.getRequestDispatcher("WEB-INF/view/result.jsp")
+                        .forward(req, resp);
+            } catch (ParserException e) {
+                LOGGER.error("error in controller", e);
+            }
+        } else {
+            req.getRequestDispatcher("WEB-INF/view/result.jsp")
+                    .forward(req, resp);
+        }
+    }
+}
